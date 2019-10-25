6675fa096b1828ec9dd9c226d38375837ba99f9d
Guilherme Ferreira <grsantos@latam.stefanini.com>
2019-10-25 09:27:50 -0300

    Criacao de rotas no front e criacao da estrutura do angular

--- Commerce/commerce-front-app/src/app/app.module.ts
+++ Commerce/commerce-front-app/src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './modules/cadastro/cadastro.component';
+import { HomeComponent } from './modules/home/home.component';
 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent, pathMatch: 'full'},
+  { path: 'cadastro', component: CadastroComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    FormsModule,
     BrowserModule
   ],
   providers: [],
